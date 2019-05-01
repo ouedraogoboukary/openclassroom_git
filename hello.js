9cece4a5b6d3dc37280fa27bc4bf2d6e4aa7039a
Boukary <abhou16@gmail.com>
2019-05-01 20:02:23 +0200

    mise en pose et reprise de programme dans la branche master

--- hello.js
+++ hello.js
@@ -1,7 +1,17 @@
+<<<<<<< Updated upstream
 var message="hello";
 alert(message);
 function afficher(){
 	var affiche="Rien de tout cela n'aura lieu"
 	return affiche
+=======
+<<<<<<< HEAD
+alert("hello")
+alert("hello pour la deuxième fois")
+alert("coucou")
+function a(){
+	var message="Bonjour tout le monde "
+	return message
+>>>>>>> Stashed changes
 }
 
